1777d32a9f981ee7574049841198a998ed9379a4
Shashinda2001 <142221873+Shashinda2001@users.noreply.github.com>
2024-06-12 15:08:17 +0530

    get only one user using id backend get operation added

--- my-app-backend/Server/routes/user.js
+++ my-app-backend/Server/routes/user.js
@@ -33,6 +33,28 @@ router.post("/users/login", async (req,res)=>{
     catch(error){
      res.status(401).send();
     }
-})
+});
+
+
+//get one user
+router.get("/users/me",auth, async (req,res)=>{    //auth dammama passe
+
+
+    // const _id= req.params.id;
+    const _id=req.user._id;  //req eke user pass wana nisa auth eken
+
+    try{
+        const user=await User.findById(_id);
+
+        if(!user){
+            return res.status(404).send();
+        }
+
+        res.status(200).send(user);
+
+    }catch(error){
+        res.status(400).send(error);
+    }
+});
 
 module.exports =router;
